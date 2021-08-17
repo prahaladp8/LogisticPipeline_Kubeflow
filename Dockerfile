96fe5e415bb2d6a00d1eceaa651576dd77a1e762
prahalad prabhakar <prahalad@prahalads-MacBook-Pro.local>
2021-08-17 23:31:43 +0530

    Refactoring code for kbp

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM prahaladp8/lrbase:3.0
 RUN rm -rf LR-Prepare 
+RUN rm -rf LR 
 RUN mkdir LR-Prepare
 WORKDIR /LR-Prepare
 #RUN cd LR-Prepare
@@ -7,21 +8,21 @@ WORKDIR /LR-Prepare
 
 #RUN pip install --no-cache-dir -r requirements.txt
 
-#RUN apt-get install -y git
-#RUN mkdir /root/.ssh/
-#ADD id_rsa /root/.ssh/id_rsa
-# Create known_hosts
-#RUN touch /root/.ssh/known_hosts
-# Add bitbuckets key
-#RUN ssh-keyscan bitbucket.org >> /root/.ssh/known_hosts
-# Clone the conf files into the docker container
-#RUN git clone git@bitbucket.org:User/repo.git
+RUN apt-get install -y git
+RUN git clone https://github.com/prahaladp8/LogisticPipeline_Kubeflow.git
+RUN cd LogisticPipeline_Kubeflow
+WORKDIR /LR-Prepare/LogisticPipeline_Kubeflow
+RUN mkdir input
+RUN cd input
+RUN wget 'https://www.dropbox.com/s/tq3xz0piqitnc59/loan_data_2007_2014.csv?dl=0' -O ./LR-Prepare/LogisticPipeline_Kubeflow/input/train.csv
+RUN wget 'https://www.dropbox.com/s/z77a2qwch6xgsy3/loan_data_2015.csv?dl=0' -O ./LR-Prepare/LogisticPipeline_Kubeflow/input/test.csv
+WORKDIR /LR-Prepare/LogisticPipeline_Kubeflow
 
-RUN mkdir base
-RUN mkdir inputs
-RUN mkdir outputs
-COPY base base
-COPY Driver.py .
+#RUN mkdir base
+#RUN mkdir inputs
+#RUN mkdir outputs
+#COPY base base
+#COPY Driver.py .
 #CMD ["python", "Driver.py","--configpath","PipelineInputs.py","--outputpath","op"] 
 
 
--- Driver.py
+++ Driver.py
@@ -9,17 +9,22 @@ def pre_execute():
 
     parser = argparse.ArgumentParser(description='Reading command line arguments for Pipeline')
     parser.add_argument('--inputpath', type=str, help='Input dir')
-    parser.add_argument('--stage', type=str, help='Output dir each component')
+    parser.add_argument('--stage', type=str, help='Stage tobe executed')
+    parser.add_argument('--outputpath', type=str, help='Output dir each component')
     args = parser.parse_args()
 
     inputpath = args.inputpath
     stage = args.stage
+    outputpath = args.outputpath
 
     if not os.path.exists(os.getcwd()+'/inputs'):
         os.mkdir(os.getcwd()+'/inputs')
 
     if not os.path.exists(os.getcwd()+'/outputs'):
         os.mkdir(os.getcwd()+'/outputs')
+
+    if not os.path.exists(outputpath):
+        os.mkdir(outputpath)
 
     if not os.path.exists(os.getcwd()+'/base/Data'):
         os.mkdir(os.getcwd()+'/base/Data')
@@ -31,8 +36,10 @@ def pre_execute():
     dest_loc = os.getcwd()+'/base/Data'
     for item in os.listdir(inputpath):
         shutil.copy2(inputpath+'/'+item, dest_loc)
+
+    #look to accept yaml file also
     
-    return {'inputpath': inputpath,'stage': stage} 
+    return {'inputpath': inputpath,'stage': stage,'outputpath':outputpath} 
 
 if __name__ == '__main__':
     try:
@@ -46,9 +53,13 @@ if __name__ == '__main__':
         elif pre_execute_dict['stage'] == "feature_reduction":
             lr_pipeline.feature_reduction(pre_execute_dict['inputpath'],os.getcwd()+'/outputs')
         elif pre_execute_dict['stage'] == "model_building":
-            pass
+            lr_pipeline.model(pre_execute_dict['inputpath'],os.getcwd()+'/outputs')
         elif pre_execute_dict['stage'] == "fine_tuning":
             pass
+        
+        local_output_loc = os.getcwd()+'/outputs'
+        for item in os.listdir(local_output_loc):
+           shutil.copy2(local_output_loc+'/'+item, pre_execute_dict['outputpath'])
 
     except Exception as e:
         print(e)
